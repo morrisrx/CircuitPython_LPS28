32ffb229c15976d83dfd9d290d42681961d54547
Richard D Morris <morrisrx@icloud.com>
2024-02-10 12:11:30 -0800

    lps28.py: Cleaned up logic that initializes and calculates pressure. Renamed registers to more closely follow the datasheet.

--- lps28.py
+++ lps28.py
@@ -25,18 +25,38 @@ except ImportError:
     pass
 
 
-__version__ = "0.0.0+auto.0"
+__version__ = "0.1.1"
 __repo__ = "https://github.com/jposada202020/CircuitPython_LPS28.git"
 
 
-_REG_WHOAMI = const(0x0F)
-_INTERRUPT_CFG = const(0xB0)
-_CTRL_REG1 = const(0x10)
-_CTRL_REG2 = const(0x11)
-_INT_SOURCE = const(0x24)
-_PRESS_OUT_XL = const(0x28)
-_TEMP_OUT_L = const(0x2B)
-_THS_P_L = const(0x0C)
+# Registers below amed according tot he ilps28qsw.pdf datasheet
+_REG_INTERRUPT_CFG = const(0x0B)
+_REG_THS_P_L       = const(0x0C)
+_REG_THS_P_H       = const(0x0D)
+_REG_IF_CTRL       = const(0x0E)
+_REG_WHOAMI        = const(0x0F)
+_REG_CTRL_REG1     = const(0x10)
+_REG_CTRL_REG2     = const(0x11)
+_REG_CTRL_REG3     = const(0x12)
+_REG_FIFO_CTRL     = const(0x14)
+_REG_FIFO_WTM      = const(0x15)
+_REG_REF_P_L       = const(0x16)
+_REG_REF_P_H       = const(0x17)
+_REG_I3C_IF_CTRL   = const(0x19)
+_REG_RPDS_L        = const(0x1A)
+_REG_RPDS_H        = const(0x1B)
+_REG_INT_SOURCE    = const(0x24)
+_REG_FIFO_STATUS1  = const(0x25)
+_REG_FIFO_STATUS2  = const(0x26)
+_REG_STATUS        = const(0x27)
+_REG_PRESS_OUT_XL  = const(0x28)
+_REG_PRESS_OUT_L   = const(0x29)
+_REG_PRESS_OUT_H   = const(0x2A)
+_REG_TEMP_OUT_L    = const(0x2B)
+_REG_TEMP_OUT_H    = const(0x2C)
+_REG_FIFO_DATA_OUT_PRESS_XL = const(0x78)
+_REG_FIFO_DATA_OUT_PRESS_L  = const(0x79)
+_REG_FIFO_DATA_OUT_PRESS_H  = const(0x7A)
 
 
 # Data Rate
@@ -109,39 +129,39 @@ class LPS28:
 
     """
 
-    _device_id = ROUnaryStruct(_REG_WHOAMI, "B")
+    _reg_device_id       = ROUnaryStruct(_REG_WHOAMI, "B")
 
-    _raw_temperature = ROUnaryStruct(_TEMP_OUT_L, "<h")
-    _raw_pressure = ROBits(24, _PRESS_OUT_XL, 0, 3)
+    _reg_raw_temperature = ROUnaryStruct(_REG_TEMP_OUT_L, "<h")
+    _reg_raw_pressure    = ROBits(24, _REG_PRESS_OUT_XL, 0, 3)
 
-    _pressure_threshold = UnaryStruct(_THS_P_L, "<H")
+    _reg_pressure_threshold = UnaryStruct(_REG_THS_P_L, "<H")
 
     # INT_SOURCE(0x24)
     # | BOOT-ON | 0 | 0 | 0 | 0 | IA | PL | PH |
-    _pressure_low = RWBit(_INTERRUPT_CFG, 1)
-    _pressure_high = RWBit(_INTERRUPT_CFG, 0)
+    _reg_pressure_low    = RWBit(_REG_INT_SOURCE, 1)
+    _reg_pressure_high   = RWBit(_REG_INT_SOURCE, 0)
 
     # INT status registers (0xB0)
     # | AUTOREFP | RESET_ARP | AUTOZERO | RESET_AZ | ---- | LIR | PLE | PHE |
-    _interrupt_low = RWBit(_INTERRUPT_CFG, 1)
-    _interrupt_high = RWBit(_INTERRUPT_CFG, 0)
+    _reg_interrupt_low   = RWBit(_REG_INTERRUPT_CFG, 1)
+    _reg_interrupt_high  = RWBit(_REG_INTERRUPT_CFG, 0)
 
     # Register CTRL_REG1 (0x10)
     # | ---- | ODR(3) | ODR(2) | ODR(1) | ODR(0) | AVG(2) | AVG(1) | AVG(0)|
-    _resolution = RWBits(3, _CTRL_REG1, 0)
-    _data_rate = RWBits(4, _CTRL_REG1, 3)
+    _reg_resolution      = RWBits(3, _REG_CTRL_REG1, 0)
+    _reg_data_rate       = RWBits(4, _REG_CTRL_REG1, 3)
 
     # Register CTRL_REG2 (0x11)
-    # | BOOT | FS_MODE | LFPF_CFG | EN_LPFP | BDU | SWRESET | ---- | ONESHOOT|
-    _full_scale = RWBits(1, _CTRL_REG2, 6)
+    # | BOOT | FS_MODE | LFPF_CFG | EN_LPFP | BDU | SWRESET | ---- | ONESHOT|
+    _reg_full_scale      = RWBit(_REG_CTRL_REG2, 6)
 
     def __init__(self, i2c_bus: I2C, address: int = 0x5D) -> None:
         self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
 
-        if self._device_id != 0xB4:
+        if self._reg_device_id != 0xB4:
             raise RuntimeError("Failed to find LPS28")
 
-        self._data_rate = RATE_10_HZ
+        self._reg_data_rate = RATE_10_HZ
         self._pressure_scale = 4096
 
     @property
@@ -182,13 +202,13 @@ class LPS28:
             "RATE_100_HZ",
             "RATE_200_HZ",
         )
-        return values[self._data_rate]
+        return values[self._reg_data_rate]
 
     @data_rate.setter
     def data_rate(self, value: int) -> None:
         if value not in data_rate_values:
             raise ValueError("Value must be a valid data_rate setting")
-        self._data_rate = value
+        self._reg_data_rate = value
 
     @property
     def resolution(self) -> None:
@@ -222,13 +242,13 @@ class LPS28:
             "RES_128",
             "RES_512",
         )
-        return values[self._resolution]
+        return values[self._reg_resolution]
 
     @resolution.setter
     def resolution(self, value: int) -> None:
         if value not in resolution_values:
             raise ValueError("Value must be a valid resolution setting")
-        self._resolution = value
+        self._reg_resolution = value
 
     @property
     def full_scale(self) -> None:
@@ -246,7 +266,7 @@ class LPS28:
 
         """
         values = ("NORMAL", "FULL_SCALE")
-        return values[self._resolution]
+        return values[self._reg_full_scale]
 
     @full_scale.setter
     def full_scale(self, value: int) -> None:
@@ -254,75 +274,72 @@ class LPS28:
             raise ValueError("Value must be a valid scale setting")
         options = (4096, 2048)
 
-        self._full_scale = options[value]
+        self._pressure_scale = options[value]
+        self._reg_full_scale = value
 
     @property
     def pressure(self) -> float:
         """
         Pressure value in hPa
         """
-
-        pressure_reading = self._raw_pressure
-        raw_pressure = self._twos_comp(pressure_reading, 24) / self._pressure_scale
-
-        return raw_pressure / 100
+        return self._twos_comp(self._reg_raw_pressure, 24) / self._pressure_scale
 
     @property
     def temperature(self) -> float:
         """The current temperature measurement in Celsius"""
 
-        return self._raw_temperature / 100
+        return self._reg_raw_temperature / 100
 
     @property
     def high_threshold_enabled(self) -> bool:
         """Set to `True` or `False` to enable or disable the high pressure threshold"""
         value = {0: False, 1: True}
-        return value[self._interrupt_high]
+        return value[self._reg_interrupt_high]
 
     @high_threshold_enabled.setter
     def high_threshold_enabled(self, value: bool) -> None:
         if value not in (True, False):
             raise ValueError("value must be a valid setting")
-        self._interrupt_high = value
+        self._reg_interrupt_high = value
 
     @property
     def low_threshold_enabled(self) -> bool:
         """Set to `True` or `False` to enable or disable the low pressure threshold."""
         value = {0: False, 1: True}
-        return value[self._interrupt_low]
+        return value[self._reg_interrupt_low]
 
     @low_threshold_enabled.setter
     def low_threshold_enabled(self, value: bool) -> None:
         if value not in (True, False):
             raise ValueError("value must be a valid setting")
-        self._interrupt_low = value
+        self._reg_interrupt_low = value
 
     @property
     def high_threshold_exceeded(self) -> bool:
         """Returns `True` if the pressure high threshold has been exceeded.
         Must be enabled by setting :attr:`high_threshold_enabled` to `True`
         and setting a :attr:`pressure_threshold`."""
-        return self._pressure_high
+        return self._reg_pressure_high
 
     @property
     def low_threshold_exceeded(self) -> bool:
         """Returns `True` if the pressure low threshold has been exceeded.
         Must be enabled by setting :attr:`high_threshold_enabled`
         to `True` and setting a :attr:`pressure_threshold`."""
-        return self._pressure_low
+        return self._reg_pressure_low
 
     @property
     def pressure_threshold(self) -> float:
         """The high pressure threshold. Use :attr:`high_threshold_enabled`
         or :attr:`high_threshold_enabled` to use it"""
         options = (16, 8)
-        return self._pressure_threshold / options[self._full_scale]
+        return self._reg_pressure_threshold / options[self._reg_full_scale]
 
     @pressure_threshold.setter
     def pressure_threshold(self, value: float) -> None:
         """The high value threshold"""
         options = (16, 8)
-        self._pressure_threshold = value * options[self._full_scale]
+        self._reg_pressure_threshold = value * options[self._reg_full_scale]
 
     @staticmethod
     def _twos_comp(val: int, bits: int) -> int:
